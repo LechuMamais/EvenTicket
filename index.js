20ecb35063d64c1ac18ef22104d6c7942172bf99
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2024-03-21 13:49:53 +0100

    Fixing CORS: Added a middleware for setting headers

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ app.use(express.json());
 
 // Middleware para habilitar CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173, http://localhost:5173/');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Max-Age', '3600'); // Tiempo en segundos
     res.setHeader('Access-Control-Allow-Credentials', true);
     if (req.method === 'OPTIONS') {
         res.sendStatus(200); // Responder con éxito a las solicitudes OPTIONS

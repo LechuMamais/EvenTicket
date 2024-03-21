2bc7323207b3f5ba804961980ac0a552a4c1310e
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2024-03-21 12:53:00 +0100

    Fixing CORS_9

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 const config = {
-    server: [
+    server: 
         {
             origin: 'https://even-ticket-j1bwy96gt-lechumamais-projects.vercel.app', //servidor que deseas que consuma o (*) en caso que sea acceso libre
             credentials: true
         }
-    ]
+    
 }
